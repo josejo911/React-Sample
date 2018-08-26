498db9661ab21348b5cf18687009c72fb0ee2983
Jose Jo Escobar <jo14343@uvg.edu.gt>
2018-08-25 22:39:13 -0600

    React Sample

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const element = React.createElement('h1',null, 'HEELLO FROM THE OTHER SIDE ... WELL REACT')
+
+ReactDOM.render(
+	element,
+	document.getElementById('root')
+)
+console.log("que ondas")
+console.log("jejejej")
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const config ={
+	mode:'development',
+	output: {
+		publicPath: '/dist',
+
+	},
+
+	module: {
+		rules: [{
+			test: /\.jsx?$/,
+			use: ['babel-loader']
+		}]
+	}
+}
+module.exports = config
+
